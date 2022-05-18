ad8db88cde9007eacbe48be72ab98f8437f5508f
Noah8922 <juyeolgo@gmail.com>
2022-05-18 11:10:08 +0900

    Network fetch by using Postman, return is essectial don't forget it

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,25 @@
-import "./app.css";
+import { useEffect, useState } from "react";
+import "./App.css";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
-  return <h1>Hello :)</h1>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBPFfo6CusHteOMipoJgHKK0VYbicES7qE",
+      requestOptions
+    )
+      .then((response) => response.json()) // 원래는 text인데 작업하기엔 json이 편함.
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+  return <VideoList videos={videos} />;
 }
 
 export default App;
