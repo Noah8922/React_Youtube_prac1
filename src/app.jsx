759571fe7c75f35bef6687dd1b7f26e090ae31c6
Noah8922 <juyeolgo@gmail.com>
2022-05-19 19:56:23 +0900

    if videos are selected, put the list on right side & detail on left side

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import styles from "./app.module.css";
 import SearchBar from "./components/search_bar/search_bar";
+import VideoDetail from "./components/video_detail/video_detail";
 import VideoList from "./components/video_list/video_list";
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
   /** 주의
    * 여기서 Youtube 클래스를 만드는 경우가 있다.
@@ -12,6 +14,10 @@ function App({ youtube }) {
    * 1. App이라는 함수를 호출 할 때마다 새로운 youtube라는 오브젝트를 만든다는 점
    * 2. 나중에 mock class를 제공하기 어렵다. (유닛테스트에도 네트워크 통신을 할 것이다. )
    */
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  };
 
   const search = (query) => {
     youtube
@@ -28,7 +34,20 @@ function App({ youtube }) {
   return (
     <div className={styles.app}>
       <SearchBar onSearch={search} />
-      <VideoList videos={videos} />
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? "list" : "grid"}
+          />
+        </div>
+      </section>
     </div>
   );
 }
