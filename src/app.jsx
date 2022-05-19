2dd3efa698cbc653f27c3dcdc33063d0a47e73de
Noah8922 <juyeolgo@gmail.com>
2022-05-19 17:55:21 +0900

    search function implemented

--- src/app.jsx
+++ src/app.jsx
@@ -6,6 +6,24 @@ import VideoList from "./components/video_list/video_list";
 function App() {
   const [videos, setVideos] = useState([]);
 
+  const search = (query) => {
+    var requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyBPFfo6CusHteOMipoJgHKK0VYbicES7qE`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        result.items.map((item) => ({ ...item, id: item.id.videoId }))
+      )
+      .then((items) => setVideos(items))
+      .catch((error) => console.log("error", error));
+  };
+
   useEffect(() => {
     const requestOptions = {
       method: "GET",
@@ -22,7 +40,7 @@ function App() {
   }, []);
   return (
     <div className={styles.app}>
-      <SearchBar />
+      <SearchBar onSearch={search} />
       <VideoList videos={videos} />
     </div>
   );
